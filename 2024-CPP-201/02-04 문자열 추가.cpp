6238f1b24e86e27f28413ac9ed9e8028fb63fbef
Hokkaido <garook@e-mirim.hs.kr>
2024-04-05 09:57:54 +0900

    주영 문자열 추가-C++스타일

--- 2024-CPP-201/02-04 문자열 추가.cpp
+++ 2024-CPP-201/02-04 문자열 추가.cpp
@@ -1,11 +1,12 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
+using namespace std;
 
 void main(void)
 {
-	char str[100] = "Juyoung";
+	string str = "Juyoung";
 	// 문자열 추가
-	strcat(str, " is pretty");
+	str += " is pretty";
 
-	printf("%s ", str);
+	cout << str;
 }
