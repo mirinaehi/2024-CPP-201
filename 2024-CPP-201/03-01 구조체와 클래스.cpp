4ef8513f38108f4be5c6019a3686937234a5238a
Hokkaido <garook@e-mirim.hs.kr>
2024-04-08 14:43:20 +0900

    클래스는 디폴트가 private

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -2,15 +2,15 @@
 #include <string>
 using namespace std;
 
-// 구조체는 디폴트가 public
-struct Student {
+// 클래스는 디폴트가 private
+class Student {
 	int hakbun_;
 	string name_;
 };
 
 void main(void)
 {
-	struct Student juyoung;
+	Student juyoung;
 
 	juyoung.hakbun_ = 2115;
 	juyoung.name_ = "윤주영";
