c963c117c1bce22b342fd1eb655d2bd867505afb
Hokkaido <garook@e-mirim.hs.kr>
2024-04-08 13:42:35 +0900

    주영 문자열 비교-C++스타일

---
+++ 2024-CPP-201/02-05 문자열 비교.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void main(void)
+{
+	string str = "Juyoung";
+	// 문자열 비교 
+	if (str == "Jiyoung")
+		cout << "같다";
+	else
+		cout << "다르다";
+}
