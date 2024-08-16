9f72370a1f740cb1d34be5dc7f5542203d529aec
JWP <garook@e-mirim.hs.kr>
2024-08-16 10:21:24 +0900

    동적할당 복습

---
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Animal {
+public:
+	void bark() 
+	{
+
+	}
+
+	void sleep()
+	{
+
+	}
+
+	void eat()
+	{
+
+	}
+
+private:
+	string name;
+	unsigned int age;
+};
+
+void main(void)
+{
+	Animal* ani = new Animal();
+}
