a6c277bdcea5584c33e2cf250c8a4507dc33f573
JWP <garook@e-mirim.hs.kr>
2024-08-19 14:07:17 +0900

    생성자 복습

--- 2024-CPP-201/05-02 다형성.cpp
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 class Animal {
 public:
+	Animal(string name, unsigned int age)
+		: name_(name), age_(age)
+	{
+		cout << "이름 " << name_ << endl;
+		cout << "나이 " << age_ << endl;
+	}
 	void bark() 
 	{
 		cout << "잘 짖는다" << endl;
@@ -22,13 +28,22 @@ public:
 	}
 
 private:
-	string name;
-	unsigned int age;
+	string name_;
+	unsigned int age_;
 };
+
+
+class Human : public Animal {
+public:
+
+private:
+	bool right_;
+};
+
 
 void main(void)
 {
-	Animal* ani = new Animal();
+	Animal* ani = new Animal("정민레이디", 18);
 	ani->bark();
 	ani->eat();
 	ani->sleep();
