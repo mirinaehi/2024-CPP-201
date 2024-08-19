99f734558f5cc721918167e43b0ea2c4d7891ee4
JWP <garook@e-mirim.hs.kr>
2024-08-19 13:48:01 +0900

    동적할당 복습2

--- 2024-CPP-201/05-02 다형성.cpp
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -8,17 +8,17 @@ class Animal {
 public:
 	void bark() 
 	{
-
+		cout << "잘 짖는다" << endl;
 	}
 
 	void sleep()
 	{
-
+		cout << "잘 잔다" << endl;
 	}
 
 	void eat()
 	{
-
+		cout << "잘 먹는다" << endl;
 	}
 
 private:
@@ -29,4 +29,9 @@ private:
 void main(void)
 {
 	Animal* ani = new Animal();
+	ani->bark();
+	ani->eat();
+	ani->sleep();
+
+	delete ani;
 }
