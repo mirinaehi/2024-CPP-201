d4287aafc3e23f0eee1dbe77c60928705e3c3e99
JWP <garook@e-mirim.hs.kr>
2024-09-27 08:51:34 +0900

    09-01 enum 적용 전

---
+++ 2024-CPP-201/09-01 enum.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main(void)
+{
+	// 0 : 한식, 1 : 일식, 2 : 중식
+	int style = 0;
+
+	switch (style)
+	{
+	case 0:
+		cout << "한식" << endl;
+		break;
+	case 1:
+		cout << "일식" << endl;
+		break;
+	case 2:
+		cout << "중식" << endl;
+		break;
+	}
+}
