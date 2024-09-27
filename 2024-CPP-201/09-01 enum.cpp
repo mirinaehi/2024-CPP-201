5555fb5cc3bf1f30ebbd7e8daf898b53c3976b4c
JWP <garook@e-mirim.hs.kr>
2024-09-27 09:07:20 +0900

    09-01 enum을 명시적으로

--- 2024-CPP-201/09-01 enum.cpp
+++ 2024-CPP-201/09-01 enum.cpp
@@ -10,17 +10,17 @@ enum FoodStyle {
 
 void main(void)
 {
-	int style = KOREAN;
+	int style = FoodStyle::KOREAN;
 
 	switch (style)
 	{
-	case KOREAN:
+	case FoodStyle::KOREAN:
 		cout << "한식" << endl;
 		break;
-	case JAPANESE:
+	case FoodStyle::JAPANESE:
 		cout << "일식" << endl;
 		break;
-	case CHINESE:
+	case FoodStyle::CHINESE:
 		cout << "중식" << endl;
 		break;
 	}
