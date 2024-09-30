6bfd4815b80216fae491a2d8f4a50646ff0a893e
JWP <garook@e-mirim.hs.kr>
2024-09-30 13:56:32 +0900

    09-02 범위기반 적용 후

--- 2024-CPP-201/09-02 범위기반 for문.cpp
+++ 2024-CPP-201/09-02 범위기반 for문.cpp
@@ -7,6 +7,6 @@ using namespace std;
 void main(void)
 {
 	vector<int> arr = { 201, 202, 203, 204 };
-	for (int i = 0; i < arr.size(); i++)
-		cout << arr[i];
+	for (int x : arr)
+		cout << x << " ";
 }
