90070c8a585a80cedabc7abefe76217c7bbf59d6
JWP <garook@e-mirim.hs.kr>
2024-10-14 13:48:07 +0900

    09-03 auto 적용 후

--- 2024-CPP-201/09-03 auto.cpp
+++ 2024-CPP-201/09-03 auto.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 void main(void)
 {
-	int a = 40;
+	auto a = 40;
 	cout << a << endl;
 
 	vector<string> v = { "허니 지혜", "심정민 lady", "윤주영 베이비"};
-	string b = v[2];
+	auto b = v[2];
 	cout << b << endl;
 }
