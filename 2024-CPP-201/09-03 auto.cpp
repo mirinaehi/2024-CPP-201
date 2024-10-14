18e8345f08c5b9580fabeae534f846cd6c1ee85e
JWP <garook@e-mirim.hs.kr>
2024-10-14 13:40:57 +0900

    09-03 auto 적용 전

---
+++ 2024-CPP-201/09-03 auto.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+void main(void)
+{
+	int a = 40;
+	cout << a << endl;
+
+	vector<string> v = { "허니 지혜", "심정민 lady", "윤주영 베이비"};
+	string b = v[2];
+	cout << b << endl;
+}
