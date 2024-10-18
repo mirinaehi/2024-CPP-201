10ee44055ee74023a362e64b5066cdf1d6fbedc6
JWP <garook@e-mirim.hs.kr>
2024-10-18 10:13:49 +0900

    [pj] 상수 처리방법3

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,12 +3,15 @@
 using namespace sf;
 using namespace std;
 
-const int WINDOW_WIDTH = 1200;
-const int WINDOW_HEIGHT = 800;
+enum App {
+	WIDTH = 1200,
+	HEIGHT = 800
+};
+
 
 void main(void)
 {
-	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Project");
+	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
 
 	while (window.isOpen())
 	{
@@ -21,7 +24,7 @@ void main(void)
 
 		RectangleShape player;
 		player.setSize(Vector2f(200.f, 200.f));
-		player.setPosition((1200-200)/2, (800-200)/2);
+		player.setPosition((App::WIDTH-player.getSize().x)/2, (App::HEIGHT-player.getSize().y)/2);
 		player.setFillColor(Color::Magenta);
 
 		window.clear(Color::White);
