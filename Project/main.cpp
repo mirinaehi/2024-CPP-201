3d004bfa56a0f8b2163959a03f16e9b0f5d2f954
JWP <garook@e-mirim.hs.kr>
2024-10-21 13:49:44 +0900

    [pj]플레이어 이동

--- Project/main.cpp
+++ Project/main.cpp
@@ -13,6 +13,11 @@ void main(void)
 {
 	RenderWindow window(VideoMode(App::WIDTH, App::HEIGHT), "Project");
 
+	RectangleShape player;
+	player.setSize(Vector2f(200.f, 200.f));
+	player.setPosition((App::WIDTH - player.getSize().x) / 2, (App::HEIGHT - player.getSize().y) / 2);
+	player.setFillColor(Color::Magenta);
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -22,10 +27,16 @@ void main(void)
 				window.close();
 		}
 
-		RectangleShape player;
-		player.setSize(Vector2f(200.f, 200.f));
-		player.setPosition((App::WIDTH-player.getSize().x)/2, (App::HEIGHT-player.getSize().y)/2);
-		player.setFillColor(Color::Magenta);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+			player.move(10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+			player.move(-10.f, 0.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Up))
+			player.move(0.f, -10.f);
+		if (Keyboard::isKeyPressed(Keyboard::Key::Down))
+			player.move(0.f, 10.f);
+
+
 
 		window.clear(Color::White);
 		window.draw(player);
